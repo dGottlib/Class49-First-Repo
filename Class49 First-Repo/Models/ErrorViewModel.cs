be9004e10cfccc2cf2f8ccce932bc1e97e97fc80
Devorah Gottlib <devorahgottlib123@gmail.com>
2022-03-14 21:22:58 -0400

    Add project files.

---
+++ Class49 First-Repo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Class49_First_Repo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
